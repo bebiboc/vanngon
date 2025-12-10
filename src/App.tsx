d89e2641614654712258574b0203753c92aaf3d9
bebiboc <129489365+bebiboc@users.noreply.github.com>
2025-12-10 08:02:24 +0100

    Set BrowserRouter basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/food-saver-hub">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/coming-soon" element={<ComingSoon />} />
